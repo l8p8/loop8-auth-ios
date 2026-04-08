43b162448100648bf3049cf828f3971d82a94fc8
John <john.harutyunyan1995@gmail.com>
2026-04-08 19:56:23 +0400

    Update Package.swift and Example app to use release 0.1.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Loop8AuthSDK",
-            url: "https://github.com/l8p8/loop8-auth-ios/releases/download/1.0.0/Loop8AuthSDK.xcframework.zip",
+            url: "https://github.com/l8p8/loop8-auth-ios/releases/download/0.1.0/Loop8AuthSDK.xcframework.zip",
             checksum: "c3d715c4c4b95dcd14a8f11ba1652e870219d4e744701575d2daa365006403b0"
         )
     ]
